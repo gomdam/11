9fcfab350d585e65560e1b1692c933f49a54ae1b
gommdamm <goyoudam2357@gmail.com>
2025-11-13 15:56:15 +0900

    11-5

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb = "all that glisters is not gold.";
-void setPointer(char **q)
-{
-	*q = proverb;
-	
-}
+
 
 int main(int argc, char *argv[]) 
 {
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%s\n", p);
+	char *pc;
+	int *pi;
+	double *pd;
 	
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
+	
+	printf("before : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("after : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	return 0;
 }
